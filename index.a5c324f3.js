596afa93aea803142fe150d3ee0bb68b81e77144
Dominika Karmel <dkarmel@op.pl>
2023-11-28 22:28:32 +0100

    make build

---
+++ index.a5c324f3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=(e.offsetWidth-t.offsetWidth)/2,f=(e.offsetHeight-t.offsetHeight)/2;t.style.left="".concat(o,"px"),t.style.top="".concat(f,"px");
+//# sourceMappingURL=index.a5c324f3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const centerX = (wall.offsetWidth - spider.offsetWidth) / 2;
+const centerY = (wall.offsetHeight - spider.offsetHeight) / 2;
+spider.style.left = `${centerX}px`;
+spider.style.top = `${centerY}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
